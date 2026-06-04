810a73ce7a81fde87acf00b6252ca5785c7fb4a2
Nivel-l <kevinjin099@gmail.com>
2026-06-04 23:57:20 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        Labirinto l = new Labirinto();
-        l.CaricaMappa();
-        l.DisegnaMappa();
-        l.Gioco();
-    }
-}
